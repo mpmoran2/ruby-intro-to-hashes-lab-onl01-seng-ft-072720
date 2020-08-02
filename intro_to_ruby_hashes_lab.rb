721fc47175a32b90acf442969557a706f33dc9e4
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-02 00:29:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
       
       
       :pennsylvania_railroad =>
-      {------------------
+      {"mortgage_value"=>"$200"
         
       },
       :b_and_o_railroad=>
