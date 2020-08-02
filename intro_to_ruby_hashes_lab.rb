a269d5dc74eb91a1f852ccd79939ad358c28d46e
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-02 00:28:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,17 +19,15 @@ def monopoly_with_third_tier
 	monopoly = {:railroads => {
 	  :pieces => 4,
 	  :rent_in_dollars => {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },:names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
+      :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
+      :four_pieces_owned =>200
+    },
+    :names=>{:reading_railroad =>
+      {"mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
         "mortgage_value"=>"$200"
       },
@@ -41,7 +39,8 @@ def monopoly_with_third_tier
       {
         "mortgage_value"=>"$800"
       }
-	}}
+	  }
+	}
 
 end
 
