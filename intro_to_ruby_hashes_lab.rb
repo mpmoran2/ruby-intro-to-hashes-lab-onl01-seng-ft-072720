dea64edea20cb1aff89bf8c719fa895b05dec142
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-02 00:29:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,11 +32,11 @@ def monopoly_with_third_tier
       {"mortgage_value"=>"$200"},
         
       
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
+        "mortgage_value"=>"$400"},
+      
+      :shortline =>
       {
         "mortgage_value"=>"$800"
       }
